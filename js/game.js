96d12d349f4126c8ba998fd1fa95056ae835fb6c
Harkirat Singh <harkirat96@gmail.com>
2015-05-15 11:00:35 +0530

    Added lower constraints to game along with their collision detectors

--- js/game.js
+++ js/game.js
@@ -42,6 +42,31 @@ upper3Image.onload = function() {
 }
 upper3Image.src="images/upper.png";
 
+//first lower bar
+var lower1Ready=false;
+var lower1Image = new Image();
+lower1Image.onload = function() {
+	lower1Ready=true;
+}
+lower1Image.src="images/lower.png";
+
+
+//second lower bar
+var lower2Ready=false;
+var lower2Image = new Image();
+lower2Image.onload = function() {
+	lower2Ready=true;
+}
+lower2Image.src="images/lower.png";
+
+//third lower bar
+var lower3Ready=false;
+var lower3Image = new Image();
+lower3Image.onload = function() {
+	lower3Ready=true;
+}
+lower3Image.src="images/lower.png";
+
 var bird = {
 xspeed : 0,
 yspeed : 0,
@@ -67,6 +92,21 @@ var upper3 = {
 	y : -70
 }
 
+var lower1 = {
+	xspeed : -30,
+	x : 20,
+	y : 150
+}
+var lower2 = {
+	xspeed : -30,
+	x : 75,
+	y : 135
+}
+var lower3 = {
+	xspeed : -30,
+	x : 130,
+	y : 160
+}
 var keysDown = {};
 //adding key listeners
 addEventListener("keydown", function (e) {
@@ -91,6 +131,7 @@ var difficulty=-40;
 // function that is called a lot
 var update = function (modifier) 
 {
+	bird.score+=modifier;
 	if (38 in keysDown && f==0) 
 	{ // Player holding up
 		bird.yspeed = -100;
@@ -103,20 +144,38 @@ var update = function (modifier)
 	upper1.x=upper1.x + upper1.xspeed * modifier;
 	upper2.x=upper2.x + upper2.xspeed * modifier;
 	upper3.x=upper3.x + upper3.xspeed * modifier;
+	lower1.x=lower1.x + lower1.xspeed * modifier;
+	lower2.x=lower2.x + lower2.xspeed * modifier;
+	lower3.x=lower3.x + lower3.xspeed * modifier;
 	if (upper1.x<-25)
 	{
-		upper1.y=difficulty-Math.random()*50;
+		upper1.y=difficulty+10-Math.random()*50;
 		upper1.x=144
 	}
 	if (upper2.x<-25)
 	{
 		upper2.x=144
-		upper2.y=difficulty-Math.random()*50;
+		upper2.y=difficulty+10-Math.random()*50;
 	}
 	if (upper3.x<-25)
 	{
 		upper3.x=144
-		upper3.y=difficulty-Math.random()*50;	
+		upper3.y=difficulty+10-Math.random()*50;	
+	}	
+	if (lower1.x<-25)
+	{
+		lower1.y=-difficulty+160-Math.random()*50;
+		lower1.x=144
+	}
+	if (lower2.x<-25)
+	{
+		lower2.x=144
+		lower2.y=-difficulty+160-Math.random()*50;
+	}
+	if (lower3.x<-25)
+	{
+		lower3.x=144
+		lower3.y=-difficulty+160-Math.random()*50;	
 	}	
 	//collision detection
 	if (bird.y>256)
@@ -128,6 +187,22 @@ var update = function (modifier)
 		reset();
 
 	if (upper3.x<15 && bird.y<upper3.y+135)
+		reset();
+	if (upper1.x<15 && bird.y<upper1.y+135)
+		reset();
+
+	if (upper2.x<15 && bird.y<upper2.y+135)
+		reset();
+
+	if (upper3.x<15 && bird.y<upper3.y+135)
+		reset();
+	if (lower1.x<15 && bird.y>lower1.y-10)
+		reset();
+
+	if (lower2.x<15 && bird.y>lower2.y-10)
+		reset();
+
+	if (lower3.x<15 && bird.y>lower3.y-10)
 		reset();
 };
 
@@ -152,12 +227,24 @@ var render = function () {
 	{
 		ctx.drawImage(upper3Image,upper3.x,upper3.y);
 	}
+	if (lower1Ready)
+	{
+		ctx.drawImage(lower1Image,lower1.x,lower1.y);
+	}
+	if (lower2Ready)
+	{
+		ctx.drawImage(lower2Image,lower2.x,lower2.y);
+	}
+	if (upper3Ready)
+	{
+		ctx.drawImage(lower3Image,lower3.x,lower3.y);
+	}
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("score: " + bird.score, 32, 32);
+	ctx.fillText("score: " +bird.score, 12, 32);
 };
 
 // the main loop of the game
